184b123ecf6ebd40a45d848766abb53b9175f9c6
Eric <eric@droplet1.eric-tang.com>
2018-01-04 19:20:38 +0800

    Extracted inline URLs to constants

--- download-cr2.bash
+++ download-cr2.bash
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+NAME='cr2'
+URL='http://do5oq4kz0w1x.cloudfront.net/radio-HTTP/cr2-hd.3gp/playlist.m3u8'
+DATA_DIR='hls-segments'
+#cr1 http://do5oq4kz0w1x.cloudfront.net/radio-HTTP/cr1-hd.3gp/playlist.m3u8
+#cr2 http://do5oq4kz0w1x.cloudfront.net/radio-HTTP/cr2-hd.3gp/playlist.m3u8
+
 # Take about 43KB per 12 seconds
 # 60 days should take about 18GB
-retain_limit=$(( 2 * 31 * 24 * 60 * 60 ))
-python hls-download.py -d data --retain_limit $retain_limit cr2 http://do5oq4kz0w1x.cloudfront.net/radio-HTTP/cr2-hd.3gp/playlist.m3u8
+retain_limit=$(( 7 * 24 * 60 * 60 ))
+python hls-download.py -d "$DATA_DIR" --retain_limit $retain_limit "$NAME" "$URL"
 
 curl -X POST -H "Content-Type: application/json" --data '{"value1":"Desktop cr2 hls recording lost."}' https://maker.ifttt.com/trigger/status/with/key/fWLSOTcW4fvNScdUAjJYB537kOzXKyKp8EFV0FpcMD7
 
